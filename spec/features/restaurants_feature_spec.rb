bf9a96e5e30e0d66f6abcfa77964207d9c005ca8
Jackrim1 <jriminton@gmail.com>
2017-01-29 21:08:10 +0000

    just started the edit pages for each restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'restaurants' do
 
   context 'no restaurants have been added' do
+
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
@@ -23,6 +24,7 @@ feature 'restaurants' do
   end
 
   context 'creating restaurants' do
+
   scenario 'prompts user to fill out a form, then displays the new restaurant' do
     visit '/restaurants'
     click_link 'Add a restaurant'
@@ -44,5 +46,22 @@ end
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
+
+  context 'editing restaurants' do
+
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Deep fried goodness'
+      click_button 'Update Restaurant'
+      click_link 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried goodness'
+      expect(current_path).to eq '/restaurants/1'
+    end
+  end
+
 
 end
